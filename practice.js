669ecaa29d99ced4d32811890396213138608fca
phenom0999 <122211010+phenom0999@users.noreply.github.com>
2023-04-08 20:44:42 +0545

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,1 +1,23 @@
-console.log("bye");
+
+// To pop manage todo when hovered over
+manage = document.querySelector("div button");  
+manage.addEventListener("mouseover", function(){
+    manage.style.borderWidth = "3px" ;
+})
+manage.addEventListener("mouseout", function(){
+    manage.style.borderWidth = "1px";
+})
+
+
+// To underline signup when hovered over
+signup = document.querySelector("a#signup");
+signup.addEventListener("mouseover", function(){
+    signup.style.borderBottom = "solid black 2px";
+})
+signup.addEventListener("mouseout", function(){
+    signup.style.borderBottom = "none";
+})
+
+
+
+
